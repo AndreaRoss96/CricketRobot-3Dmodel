dd9af609140d03939adc996d0b98e0430f6a32ff
Tuan Nguyen <tnguyen@mathworks.com>
2021-04-09 18:43:57 -0700

    corrected problem in bash script -> problems in spawns

---
+++ gazebo_spawn.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "removing ..."
+
+`rm -r ~/catkin_ws/src/cricket_robot/*`
+
+echo "copying ..."
+
+`cp -a * ~/catkin_ws/src/cricket_robot/`
+#`cp -a cricket_gazebo/ ~/catkin_ws/src/cricket_robot/`
+#`cp -a package.xml ~/catkin_ws/src/cricket_robot/`
+
+echo "setting up workspace"
+
+cd ~/catkin_ws
+
+echo "I'm in the ws"
+
+catkin_make
+. ~/catkin_ws/devel/setup.bash
+
+echo "ws set up, launch:"
+
+roslaunch cricket_robot gazebo.launch
